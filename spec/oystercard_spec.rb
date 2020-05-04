101292410e46dc65ffc3f98653eebde77fa641bd
Anna <anna.voitik@gmail.com>
2020-05-04 14:29:32 +0100

    Card can be topped up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,12 @@
 require 'oystercard'
 describe Oystercard do
   subject { Oystercard.new(0) }
-  
+
   it 'has balance of zero' do
     expect(subject.balance).to eq(0)
+  end
+
+  it 'can be topped up' do
+    expect{subject.top_up(10)}.to change {subject.balance}.by(10)
   end
 end
